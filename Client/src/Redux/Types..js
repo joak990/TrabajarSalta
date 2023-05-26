69718b18035819d0b548b49308a3bc7233ed0356
joak990 <joakhaidar@gmail.com>
2023-05-26 16:17:17 -0300

    new

--- Client/src/Redux/Types..js
+++
@@ -1,4 +0,0 @@
-export const GET_ALL_JOBS = "GET_ALL_JOBS"
-export const POST_JOB = "POST_JOB"
-export const SORT_JOBS_BY_RECENT = 'SORT_JOBS_BY_RECENT';
-export const RESET_FILTERS = "RESET_FILTERS"
