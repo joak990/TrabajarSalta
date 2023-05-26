adc0b76f1d60fdfbbf6c058c7e7c90df90645921
joak990 <joakhaidar@gmail.com>
2023-05-26 17:26:08 -0300

    deployement

--- todo/Servidor/db.js
+++ todo/Servidor/db.js
@@ -5,7 +5,9 @@ const {DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY} = process.env;
 const fs = require('fs');
 const postJobModel = require('./models/Clasificado'); 
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const db_deploy = DB_DEPLOY ? DB_DEPLOY : `postgres:${DB_USER}:${DB_PASSWORD}@${DB_HOST}/trabajarsalta`;
+
+const sequelize = new Sequelize(db_deploy, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
