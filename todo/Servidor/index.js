07beee9b8dd9885a8563fc66088029f2e71a3f14
joak990 <joakhaidar@gmail.com>
2023-05-26 17:13:45 -0300

    port

--- todo/Servidor/index.js
+++ todo/Servidor/index.js
@@ -1,9 +1,11 @@
 const server = require('./app');
 const { conn } = require('./db.js');
+require('dotenv').config();
 
+const PORT = process.env.PORT || 3001;
 conn.sync({ alter: true }).then(() => {
 
-  server.listen(3001 , async() => {
+  server.listen(PORT , async() => {
     
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
